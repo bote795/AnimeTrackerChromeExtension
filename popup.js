6d01c35cf1252e1f5e79ead6cdaf717f269c917c
bote795 <nick_n_b@yahoo.com>
2014-04-13 02:14:55 -0500

    add bootsrap, and -,+ buttons for count
    Add a nicer looking interface and ways to increase and decrease the
    episode you are in.

--- popup.js
+++ popup.js
@@ -4,21 +4,57 @@ $(document).ready( function() {
   //use an autocompletefeature to complete anime name
   //allow user to add anime ep they are in by editing the number
   var arrayOfUrls = JSON.parse(localStorage["savedAnimes"]);
-  var $target = $('.links ol');
+  var $target = $('.container .table > tbody');
 
   for (var i = 0; i < arrayOfUrls.length; i++) {
-    var string = '<li id='+ i +'>' + arrayOfUrls[i][0] + 
-                      '</a> EP: '+ arrayOfUrls[i][1]+'<button id='+ i +'>X</button></li>'
+    var string = tableRow(i,arrayOfUrls[i][0], arrayOfUrls[i][1] );
     $target.append(string);
   }
   
-  $('button').on('click', function(e) {
+  $('.container .table .btn-toolbar .btn-group button').on('click', function(e) {
     e.preventDefault();
-    var id = parseInt(e.currentTarget.id);
-    arrayOfUrls.splice(id, 1);
-    localStorage["savedAnimes"] = JSON.stringify(arrayOfUrls);
-    $('li#' + id).remove();
+    //TODO make counter update when clicked
+     if($(e.currentTarget).text() == "-")
+     {
+      var id = parseInt(e.currentTarget.id);
+      var arrayOfUrls = JSON.parse(localStorage["savedAnimes"]);
+      arrayOfUrls[id][1]--;
+      localStorage["savedAnimes"] = JSON.stringify(arrayOfUrls);
+     }
+     else if($(e.currentTarget).text() == "+")
+     {
+        var id = parseInt(e.currentTarget.id);
+        var arrayOfUrls = JSON.parse(localStorage["savedAnimes"]);
+        arrayOfUrls[id][1]++;
+        localStorage["savedAnimes"] = JSON.stringify(arrayOfUrls);
+     }
+     else if($(e.currentTarget).text() == "X")
+     {
+        var id = parseInt(e.currentTarget.id);
+        var arrayOfUrls = JSON.parse(localStorage["savedAnimes"]);
+        arrayOfUrls.splice(id, 1);
+        localStorage["savedAnimes"] = JSON.stringify(arrayOfUrls);
+        $('tr#' + id).remove();
+     }
+    else;
+    
   });
 });
+function tableRow(i, title, ep)
+{
+  return "<tr id="+ i +">"+
+   "<td>" + title + "</td>"+
+   "<td>"+
+    "<div class='btn-toolbar'>"+
+      "<div class='btn-group'>"+
+        "<button type='button' class='btn btn-default' id="+ i +">-</button>"+
+        "<button type='button' class='btn btn-default disabled' id="+ i +">"+ ep +"</button>"+
+        "<button type='button' class='btn btn-default' id="+ i +">+</button>"+
+        "<button type='button' class='btn btn-default' id="+ i +">X</button>"+
+      "</div>"+
+    "</div>"+
+    "</td>"+
+"</tr>";
+}
 
 

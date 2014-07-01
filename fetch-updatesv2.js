82347fc63361c29d74f5cadcaf3ec65332a22150
bote795 <nbotell2@gmail.com>
2014-06-30 20:36:46 -0400

    recursive update
    recursive call to a function then checking for anime

--- fetch-updatesv2.js
+++ fetch-updatesv2.js
@@ -1,115 +1,137 @@
 var urls = ["http://www.gogoanime.com/","http://www.lovemyanime.net/latest-anime-episodes/", "http://www.animefreak.tv/tracker","http://www.animeseason.com/"];
 var xpaths = ['xpath="//div[@class=\'post\']//li"',
-			  'xpath="//div[@class=\'noraml-page_in_box_mid\']//div[@class=\'noraml-page_in_box_mid_link\']//@href"',
-			  'xpath="//div[@class=\'view-content\']//tbody//tr//@href"'
-			  ,'xpath="//div[@id=\'frontpage_left_col\']//@href"'];
-  $.when
-  (
-    // loop through the different websites
-    $.when
-      ( function()
-        {
-          query = 'select * from html where url ="'+ urls[i] +'" and '+xpaths[i];
-          var yqlAPI = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(query) + ' &format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=?';
-            $.getJSON(yqlAPI, function(r){
+        'xpath="//div[@class=\'noraml-page_in_box_mid\']//div[@class=\'noraml-page_in_box_mid_link\']//@href"',
+        'xpath="//div[@class=\'view-content\']//tbody//tr//@href"'
+        ,'xpath="//div[@id=\'frontpage_left_col\']//@href"'];
+var updates= [];    
+var i=0;
+function getFeed()
+{
+  if (i < urls.length) 
+  {
+      var query = 'select * from html where url ="'+ urls[i] +'" and '+xpaths[i];
+    var yqlAPI = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(query) + ' &format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=?';
+  $.getJSON(yqlAPI, function(){
+      console.log("sucess");
+  })
+    .success(function(r){
+         if (urls[i] == "http://www.gogoanime.com/") 
+              {
+                 $.each(r.query.results.li, function(){ 
+                      if(typeof this.font !== 'undefined')
+                          {        
+                            if(this.font.content !== "(Raw)")
+                             {
+                                updates.push([this.a.href,this.font.content, this.a.href]);
+                             }
+                          }
+                    }); // close each
+              }
+              else
+              {
+                $.each(r.query.results.a, function(){ 
+                 if(typeof this.href !== 'undefined')
+                  {
+                      updates.push([this.href, "(Sub)", (urls[i]+this.href)]);
+                  }
+                }); // close each
+              } // close else
+        i++;
+         getFeed();
+      }); // close sucess
+  }
+    else 
+    isUp();
+ 
+}
+getFeed();
 
-            if (urls[i] == "http://www.gogoanime.com/") 
-            {
-               $.each(r.query.results.li, function(){ 
-                    if(typeof this.font !== 'undefined')
-                        {        
-                          if(this.font.content !== "(Raw)")
-                           {
-                              updates.push([this.a.href,this.font.content, this.a.href]);
-                           }
-                        }
-                  });
-            }
-            else
-            {
-              $.each(r.query.results.a, function(){ 
-               if(typeof this.href !== 'undefined')
-                {
-                    updates.push([this.href, "(Sub)", (urls[i]+this.href)]);
-                }
+function isUp()
+{
+  var eplog = JSON.parse(localStorage["savedAnimes"]);
+  //switch for loops for implementation  
+ for(var e = 0; e< eplog.length; e++)
+  {
+      for(var i = 0; i< updates.length; i++)
+      {
+        if(eplog[e][2]!==1 && NextEp(updates[i][0],eplog[e][0],eplog[e][1]))
+           {
+             var temp="New Episode is up for ";
+             var x ="";
+             eplog[e][2]=1;
+             eplog[e][3]=updates[i][2];
+             x=x.concat(temp,eplog[e][0]);             
+               var opt = {
+                type: "basic",
+                title: "New Episode",
+                message: x,
+                 iconUrl: "icon.png"
+              }
+              chrome.notifications.create( JSON.stringify({id: e ,url: updates[i][2] }), opt, function() {
+                  console.log("Succesfully created " + e + " notification");
               });
-            }
-          }); //close json
-        }
-      ).done
-      ( function()
-        {
-         //finish geting info
-        }
-      );
-  ).done
-  (
-      function (argument) {
-        // body...
-      }
-  );
-    
-
-
-   function function_name (argument) {
-   for (var i = urls.length - 1; i >= 0; i--) 
-   {
-   	urls[i]
-   	query = 'select * from html where url ="'+ urls[i] +'" and '+xpaths[i];
-   	var yqlAPI = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(query) + ' &format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=?';
-   		$.getJSON(yqlAPI, function(r){
-		    $.each(r.query.results.a, function(){ 
-		         if(typeof this.href !== 'undefined')
-		          {
-		            lovemyAnime1.push([this.href, "(Sub)", ("http://www.lovemyanime.net"+this.href)]);
-		          }
-		   	 });
-		}); //close json
-   }
-   
-   }
-
-  hi = "http://www.gogoanime.com/";
-query = 'select * from html where url ="'+ hi +'" and xpath="//div[@class=\'post\']//li"';
-yqlAPI = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(query) + ' &format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=?';
-$.getJSON(yqlAPI, function(r){
-    $.each(r.query.results.li, function(){ 
-      if(typeof this.font !== 'undefined')
+              chrome.notifications.onClicked.addListener(notificationClicked);
+              break;
+           }
+           else if(typeof NextEp(updates[i][0],eplog[e][0],eplog[e][1]) === 'undefined')
           {        
-            if(this.font.content !== "(Raw)")
-             {
-                gogoAnime1.push([this.a.href,this.font.content, this.a.href]);
-             }
+                console.log("NextEp: undefined returned")    
+                console.log(updates[i][0] +" : "+ eplog[e][0]+" : "+eplog[e][1] )
+          
           }
-    });
-   gotFollowersOfA(gogoAnime1);
-});
-   hi = "http://www.animeseason.com/";
-query = 'select * from html where url ="'+ hi +'" and xpath="//div[@id=\'frontpage_left_col\']//@href"';
-yqlAPI = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(query) + ' &format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=?';
-
-$.getJSON(yqlAPI, function(r){
-  var x=0;
-    $.each(r.query.results.a, function(){ 
-      x++;
-       if(typeof this.href !== 'undefined' && x%3 ===0)
+      }
+  }
+  localStorage["savedAnimes"] = JSON.stringify(eplog);
+}
+function notificationClicked(ID) {
+  var x = JSON.parse(ID);
+  console.log("clicked:"+parseInt(x.id));
+  window.open(x.url);
+  chrome.runtime.sendMessage({method: "EpUpdate",
+                                  sentUrl: x.url,
+                                  title: "x" },
+        function (response) {
+          if (response.status === 200) {
+           // $(e.target).css("color", "green");
+          }
+        });
+}
+function NextEp(url, title, ep)
+{
+    var words = [];
+    title = title.toLowerCase();
+    words =title.split(new RegExp("\\s+"));
+    if(typeof ep !== "number")
+    {
+      ep = parseInt(ep);
+    }
+    //finds a part of name and creates a substring from where that part of the url starts and the remainder of url
+      for (var i = 0; i < words.length; i++) 
+      {
+        
+        if(url.indexOf(words[i])!= -1)
+        {
+          url = url.substring(url.indexOf(words[i])+words[i].length, url.length);
+        }
+        else
+           return false;
+      }
+    //looks for episode then episode # by creating substrings
+    //if episode isn't found then it just looks for episode number to account for websites that don't write in episode
+      if(url.indexOf("episode") != -1)
+      {
+         url = url.substring(url.indexOf("episode")+ "episode".length, url.length);
+        if(url.indexOf(ep+1)!= -1)
           {
-             animeSeason1.push([this.href, "(Sub)", ("http://www.animeseason.com"+this.href)]);
-            x=0;
+            return true;
           }
-    });
-  gotFollowersOfD(animeSeason1);
-});
-hi = "http://www.animefreak.tv/tracker";
-query = 'select * from html where url ="'+ hi +'" and xpath="//div[@class=\'view-content\']//tbody//tr//@href"';
-yqlAPI = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(query) + ' &format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=?';
-
-$.getJSON(yqlAPI, function(r){
-    $.each(r.query.results.a, function(){ 
-      if(typeof this.href !== 'undefined')
+          else
+            return false;
+      }
+      else if(url.indexOf(ep+1)!= -1)
           {
-            animeFreak1.push([this.href, "(Sub)" , ("http://www.animefreak.tv"+this.href)]);
+            return true;
           }
-    });
-   gotFollowersOfC(animeFreak1);
-});
+      else 
+        return false;
+}

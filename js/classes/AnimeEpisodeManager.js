b66c3c1d5bb8cd5cb231f7731a30ebb0c6069009
bote795 <nbotell2@gmail.com>
2015-10-30 20:33:34 -0500

    put default in wrong class

--- js/classes/AnimeEpisodeManager.js
+++ js/classes/AnimeEpisodeManager.js
@@ -3,22 +3,6 @@ var AnimeEpisodeManager =  function () {
 }
 inheritsFrom(AnimeEpisodeManager, Manager);
 
-AnimeEpisodeManager.prototype.default = function() {
-	var default_values =  [
-  ["http://www.lovemyanime.net/latest-anime-episodes/",
-  'xpath="//div[@class=\'noraml-page_in_box_mid\']//div[@class=\'noraml-page_in_box_mid_link\']//@href"',
-   "html",true, "http://www.lovemyanime.net"],
-  ["http://www.animefreak.tv/tracker",
-  'xpath="//div[@class=\'view-content\']//tbody//tr//@href"',
-   "html",true, "http://www.animefreak.tv"],
-  ["http://www.animeseason.com/",
-  'xpath="//div[@id=\'frontpage_left_col\']//@href"',
-  "html",true, "http://www.animeseason.com"],
-  ["http://www.gogoanime.com/",
-  'xpath="//div[@class=\'post\']//li"',
-  "html",false, ""]
-];
-    this.save(default_values);
-};
+
 
 var AnimeEpisodeManager = new AnimeEpisodeManager(); 
